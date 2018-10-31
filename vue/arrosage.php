c5fca722633001977ae32e4ecca7427c3ab9deba
bastiengrignon <basgrignon@gmail.com>
2018-10-31 17:59:46 +0100

    Changement

--- vue/arrosage.php
+++ vue/arrosage.php
@@ -22,14 +22,39 @@
                     <img class="ajout-maison" src="../vue/images/btn-add.png" alt="">
                 </a>
             </div>
+            <!-- Requête PHP db pour afficher les arroseurs de la maison -->
             <div class="arroseur">
                 <div class="space-between">
                     <div class="nom-arroseur">
                         Arbre au fond du jardin
                     </div>
                     <div class="toggle-button">
-                        <input type="checkbox" class="arroseur-checkbox" id="arroseurID-maisonID" name="button" checked></input>
-                        <label class="arroseur-label" for="arroseurID-maisonID">
+                        <input id="maison1-arroseur1" type="checkbox" class="arroseur-checkbox" name="button" checked></input>
+                        <label class="arroseur-label" for="maison1-arroseur1">
+                            <span class="arroseur-inner"></span>
+                            <span class="arroseur-slider"></span>
+                        </label>
+                    </div>
+                </div>
+                <div class="progress">
+                    <progress value="50" max="100" class="progress-bar"></progress>
+                    <div class="progress-value strong">{{progress.value}}%</div>
+                </div>
+                <div class="space-between">
+                    <svg class="arroseur-status">
+                        <circle cx="15" cy="10" r="10" />
+                    </svg>
+                    <img src="../vue/images/info.png" alt="" width="30" height="30">
+                </div>
+            </div>
+            <div class="arroseur">
+                <div class="space-between">
+                    <div class="nom-arroseur">
+                        Pelouse
+                    </div>
+                    <div class="toggle-button">
+                        <input id="maison1-arroseur2" type="checkbox" class="arroseur-checkbox" name="button"></input>
+                        <label class="arroseur-label" for="maison1-arroseur2">
                             <span class="arroseur-inner"></span>
                             <span class="arroseur-slider"></span>
                         </label>
@@ -60,8 +85,6 @@
             </div>
             <div class="arroseur">
             </div>
-            <div class="arroseur">
-            </div>
         </div>
     </div>
     <div class="col-droite centre v-centre column">
@@ -77,21 +100,26 @@
 
 <div class="modal centre">
     <div class="modal-content">
-        <div class="modal-header">
-            <span class="close">&times;</span>
-            <h3>Ajouter un nouvel arroseur</h3>
-        </div>
-        <div class="modal-body">
-            <p>
-                <span>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repellat officia aut obcaecati reprehenderit incidunt odit.</span>
-                <span>Voluptate itaque omnis ea, illum nisi saepe aut vero dolorem, perferendis, eum cum nostrum vitae.</span>
-                <span>Beatae, odio quod iste sed maxime nobis. Excepturi odit pariatur sit eos similique molestias quam!</span>
-                <span>Nesciunt voluptatum, temporibus obcaecati tenetur ut atque amet, blanditiis fugiat quam facilis, ea quaerat repudiandae.</span>
-                <span>Non minus quaerat rem deleniti voluptates, sequi, optio eaque quam eum et repellat, delectus, laudantium.</span>
-            </p>
-        </div>
-        <div class="modal-footer">
-            <a href="" class="droite">Ajouter</a>
-        </div>
+        <form class="" action="" method="post">
+            <div class="modal-header">
+                <span class="close">&times;</span>
+                <h3>Ajouter un nouvel arroseur</h3>
+            </div>
+            <div class="modal-body">
+                <input type="text" name="house-name" placeholder="Nom de la maison">
+                <input type="number" name="house-number" placeholder="Numéro de rue">
+                <input type="text" name="house-route" placeholder="Rue">
+                <input type="text" name="house-city" placeholder="Ville">
+                <input type="text" name="house-postal" placeholder="Code Postal">
+                <input type="text" name="house-country" placeholder="Pays">
+
+            </div>
+            <div class="modal-footer droite">
+                <!-- <a href="" class="droite">Ajouter</a> -->
+                <input type="submit" name="submit" value="Ajouter">
+            </div>
+        </form>
     </div>
 </div>
+
+<?php // WARNING: Pour toggle bouton ON/OFF attention au label avec le for="idinput" ?>
