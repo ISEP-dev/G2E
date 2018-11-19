9577c124a74f16839f837ffe63741aa57a2c7618
bastiengrignon <basgrignon@gmail.com>
2018-11-19 11:42:53 +0100

    Changement

--- vue/db/process_maison.php
+++ vue/db/process_maison.php
@@ -27,7 +27,7 @@ class House
     // Récupérer la(les) maison(s) d'un utilisateur
     public static function getHouses(/*$idUser*/)
     {
-        $mysqli = new mysqli("localhost", "root", "", "g2e");
+        $mysqli = new mysqli("localhost", "root", "", "g2e_test");
         // Requête SQL
         // // WARNING: Changer requête pour afficher celles de l'utilisateur
         $housesQuery = $mysqli->query("SELECT * FROM habitation WHERE id_util = 1;");
@@ -49,48 +49,7 @@ class House
             <?php
         } # End of while
     }
-    public static function getArroseur()
-    {
-        $mysqli = new mysqli("localhost", "root", "", "g2e");
-        $arroseurQuery = $mysqli->query("SELECT * FROM arroseur WHERE id_habit = 1;");
-        while ($arroseur = $arroseurQuery->fetch_assoc()) {
-            ?>
-            <div class="arroseur">
-                <div class="space-between">
-                    <div class="nom-arroseur">
-                        <?= $arroseur['nom_arr'] ?>
-                    </div>
-                    <div class="toggle-button">
-                        <input id="<?= "maison".$arroseur['id_habit']."-arroseur".$arroseur['id_arr']; ?>" type="checkbox" class="arroseur-checkbox" name="button" checked></input>
-                        <label class="arroseur-label" for="<?= "maison".$arroseur['id_habit']."-arroseur".$arroseur['id_arr']; ?>">
-                            <span class="arroseur-inner"></span>
-                            <span class="arroseur-slider"></span>
-                        </label>
-                    </div>
-                </div>
-                <div class="progress">
-                    <progress value="50" max="100" class="progress-bar"></progress>
-                    <div class="progress-value strong">{{progress.value}}%</div>
-                </div>
-                <div class="space-between">
-                    <svg class="arroseur-status">
-                    <?php if ($arroseur['etat_arr'] == 0)
-                    {
-                    ?>
-                        <circle cx="15" cy="10" r="10" fill="red" />
-                    <?php
-                    } else { ?>
-                        <circle cx="15" cy="10" r="10" fill="green"/>
-                    <?php
-                    }?>
-                    </svg>
-                    <img src="../vue/images/info.png" alt="" width="30" height="30">
-                </div>
-            </div>
-            <?php
-        }
-    }
-
+    
     // Fonction de visualisation d'informations de la maison
 
     // Ajouter une nouvelle maison
