dc8cee092b389ac8ba1e1c68f37629848734d2eb
bastiengrignon <basgrignon@gmail.com>
2018-11-20 10:58:50 +0100

    Fonction de connexion à la base de donnée

--- modele/connexion.php
+++ modele/connexion.php
@@ -5,10 +5,9 @@ try
     $user           = "root";
     $password       = "";
     $options        = array(
-        PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8',
-        PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION
+        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
     );
-    $bdd = new PDO($dsn, $user, $password, $options);
+    $bdd = new PDO($dataSourceName, $user, $password, $options);
 }
 catch (Exception $e)
 {
