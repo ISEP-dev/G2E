dec7cb3174e455829ae198b45861590b17aefca1
bastiengrignon <basgrignon@gmail.com>
2018-11-26 17:50:25 +0100

    Ajout et modification de fonction utile pour l'échange avec la base de donnée

--- modele/fonctions.php
+++ modele/fonctions.php
@@ -26,14 +26,20 @@ function selectAll(PDO $bdd, string $table): array
 function selectWhere(PDO $bdd, string $table, array $attributs): array
 {
     $where = '';
-    foreach ($attributs as $key => $value){
-
-        $where .= "";
+    foreach($attributs as $key => $value) {
+        $where .= "$key = :$key" . ", ";
     }
-    $query = $bdd->prepare('SELECT * FROM' . $table . 'WHERE' . $where);
+    $where = substr_replace($where, '', -2, 2); //caractère de fin de chaine ? \0
+
+    $statement = $bdd->prepare('SELECT * FROM ' . $table . ' WHERE ' . $where);
 
 
-    return $query->fetchAll();
+    foreach($attributs as $key => $value) {
+        $statement->bindParam(":$key", $value);
+    }
+    $statement->execute();
+
+    return $statement->fetchAll();
 }
 
 /**
@@ -46,15 +52,28 @@ function insert(PDO $bdd, string $table, array $values): bool
 {
     $attributs = '';
     $valeurs = '';
-
-    $query = 'INSERT INTO' . $table . '(' . $attributs . ') VALUES (' . $valeurs . ')';
-    $donnees = $bdd->prepare($query);
-
-    $requete = '';
     foreach ($values as $key => $value) {
-        $requete = $requete . $key . ' : ' . $value . ',v';
+
+        $attributs .= $key . ', ';
+        $valeurs .= ':' . $key . ', ';
+        $v[] = $value;
+    }
+    $attributs = substr_replace($attributs, '', -2, 2);
+    $valeurs = substr_replace($valeurs, '', -2, 2);
+
+    $query = ' INSERT INTO ' . $table . ' (' . $attributs . ') VALUES (' . $valeurs . ')';
+
+    $donnees = $bdd->prepare($query);
+    $requete = "";
+    foreach ($values as $key => $value) {
+        $requete = $requete . $key . ' : ' . $value . ', ';
         $donnees->bindParam($key, $values[$key], PDO::PARAM_STR);
     }
+
     return $donnees->execute();
 
+}
+
+function delete(PDO $bdd, string $table, string $where){
+    $bdd->query("DELETE FROM ". $table . "WHERE " . $where);
 }
