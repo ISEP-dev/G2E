3476752dc0cbe46f528cac92b688d6a25ba6f4c6
bastiengrignon <basgrignon@gmail.com>
2018-12-14 14:24:04 +0100

    Resolving merge conflicts again... thnaks Joseph ;)

--- vue/CommercialClient.php
+++ vue/CommercialClient.php
@@ -7,105 +7,13 @@
 </div>
 <br><br>
 <div class="ligne">
-<<<<<<< HEAD
-    <!-- <div class="col-gauche">
-            <div id="formulaire">
-                <form action="index.phpt" method="post">
-                    <input type="button" value="G" style="font-weight: bold;" onclick="commande('bold');" />
-                      <input type="button" value="I" style="font-style: italic;" onclick="commande('italic');" />
-                      <input type="button" value="S" style="text-decoration: underline;" onclick="commande('underline');" />
-                      <input type="button" value="Lien" onclick="commande('createLink');" />
-                      <input type="button" value="Image" onclick="commande('insertImage');" />
-                      <select onchange="commande('heading', this.value); this.selectedIndex = 0;">
-                    <option value="">Titre</option>
-                    <option value="h1">Titre 1</option>
-                    <option value="h2">Titre 2</option>
-                    <option value="h3">Titre 3</option>
-                    <option value="h4">Titre 4</option>
-                    <option value="h5">Titre 5</option>
-                    <option value="h6">Titre 6</option>
-                      </select>
-                    <br>
-                    <textarea type="Votre message"id="message" name="Message" placeholder="Publiez votre message !"></textarea>
-                    </form></div>
-			<div id=boutton_publier><button class="boutton_publier" style="vertical-align:middle"><span> Publier </span></button></div>
-                <div id=boutton_option><button class="boutton_option" style="vertical-align:middle">... </button></div>
-                </div>
-        </div> -->
-        <div class="col-gauche">
-            <form id="" class="" action="index.html" method="post" accept-charset="UTF-8" enctype="multipart/form-data">
-                <h1 class="centre">Chercher un client</h1>
-                <hr>
-                <label for="Nom"> &nbsp;</label>
-                <input type="text" id="Nom" name="Nom" placeholder="Nom" required>
-                <label for="Prénom"> &nbsp;</label>
-                <input type="text" id="Prénom" name="Prénom" placeholder="Prénom" required>
-
-                <label for="Numéro téléphone"> &nbsp;</label>
-                <input type="text" id="Numéro téléphone" name="Numéro téléphone" placeholder="Numéro téléphone" required>
-                <br><br>
-                Résultat recherche
-                <div>
-                    <textarea name="content" rows="8" cols="80" placeholder="Résultat recherches" required></textarea>
-                    <div id="toolbar-editor">
-            <div id=boutton_publier><button class="boutton_publier" style="vertical-align:middle"><span> Publier </span></button></div>
-            <div id=boutton_option><button class="boutton_option" style="vertical-align:middle">... </button></div>
-            </div>
-    </div> -->
-||||||| merged common ancestors
-    <!-- <div class="col-gauche">
-            <div id="formulaire">
-                <form action="index.phpt" method="post">
-                    <input type="button" value="G" style="font-weight: bold;" onclick="commande('bold');" />
-                      <input type="button" value="I" style="font-style: italic;" onclick="commande('italic');" />
-                      <input type="button" value="S" style="text-decoration: underline;" onclick="commande('underline');" />
-                      <input type="button" value="Lien" onclick="commande('createLink');" />
-                      <input type="button" value="Image" onclick="commande('insertImage');" />
-                      <select onchange="commande('heading', this.value); this.selectedIndex = 0;">
-                    <option value="">Titre</option>
-                    <option value="h1">Titre 1</option>
-                    <option value="h2">Titre 2</option>
-                    <option value="h3">Titre 3</option>
-                    <option value="h4">Titre 4</option>
-                    <option value="h5">Titre 5</option>
-                    <option value="h6">Titre 6</option>
-                      </select>
-                    <br>
-                    <textarea type="Votre message"id="message" name="Message" placeholder="Publiez votre message !"></textarea>
-                    </form></div>
-			<div id=boutton_publier><button class="boutton_publier" style="vertical-align:middle"><span> Publier </span></button></div>
-                <div id=boutton_option><button class="boutton_option" style="vertical-align:middle">... </button></div>
-                </div>
-        </div> -->
-        <div class="col-gauche">
-            <form id="" class="" action="index.html" method="post" accept-charset="UTF-8" enctype="multipart/form-data">
-                <h1 class="centre">Chercher un client</h1>
-                <hr>
-                <label for="Nom"> &nbsp;</label>
-                <input type="text" id="Nom" name="Nom" placeholder="Nom" required>
-                <label for="Prénom"> &nbsp;</label>
-                <input type="text" id="Prénom" name="Prénom" placeholder="Prénom" required>
-              
-                <label for="Numéro téléphone"> &nbsp;</label>
-                <input type="text" id="Numéro téléphone" name="Numéro téléphone" placeholder="Numéro téléphone" required>
-                <br><br>
-                Résultat recherche
-                <div>
-                    <textarea name="content" rows="8" cols="80" placeholder="Résultat recherches" required></textarea>
-                    <div id="toolbar-editor">
-            <div id=boutton_publier><button class="boutton_publier" style="vertical-align:middle"><span> Publier </span></button></div>
-            <div id=boutton_option><button class="boutton_option" style="vertical-align:middle">... </button></div>
-            </div>
-    </div> -->
-=======
->>>>>>> 1dc10066862238ef4ed37e5aec67fa8d7a89047d
     <div class="col-gauche">
         <form id="" class="" action="index.html" method="post" accept-charset="UTF-8" enctype="multipart/form-data">
             <h1 class="centre">Chercher un client</h1>
             <hr>
             <label for="Nom"> &nbsp;</label>
             <input type="text" id="Nom" name="Nom" placeholder="Nom" required>
-            <label for="Prénom">&nbsp;</label>
+            <label for="Prénom"> &nbsp;</label>
             <input type="text" id="Prénom" name="Prénom" placeholder="Prénom" required>
 
             <label for="Numéro téléphone"> &nbsp;</label>
@@ -115,15 +23,14 @@
             <div>
                 <textarea name="content" rows="8" cols="80" placeholder="Résultat recherches" required></textarea>
                 <div id="toolbar-editor">
-                    </select>
+                    <div id=boutton_publier>
+                        <button class="boutton_publier" style="vertical-align:middle"><span> Publier </span></button>
+                    </div>
+                    <div id=boutton_option>
+                        <button class="boutton_option" style="vertical-align:middle">...</button>
+                    </div>
                 </div>
             </div>
-            <br><br>
-            <div class="droite">
-                <h2>
-                    <bouton class="rechercher" style="vertical-align:middle"><span>Rechercher </span></button>
-            </div>
-        </form>
     </div>
     <div class="col-droite">
         <div class="centre">
