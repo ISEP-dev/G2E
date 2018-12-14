e1dbb6761e2880f7426be651cf1450bb789397ce
bastiengrignon <basgrignon@gmail.com>
2018-12-14 14:42:03 +0100

    Resolving merge conflicts

--- vue/CommercialClient.php
+++ vue/CommercialClient.php
@@ -8,70 +8,42 @@
 <br><br>
 <div class="ligne">
     <div class="col-gauche">
-        <form id="" class="" action="index.html" method="post" accept-charset="UTF-8" enctype="multipart/form-data">
+        <form id="" class="" action="index.html" method="post" accept-charset="UTF-8"
+              enctype="multipart/form-data">
             <h1 class="centre">Chercher un client</h1>
             <hr>
             <label for="Nom"> &nbsp;</label>
             <input type="text" id="Nom" name="Nom" placeholder="Nom" required>
-            <label for="Prénom" &nbsp;</label>
+            <label for="Prénom">&nbsp;</label>
             <input type="text" id="Prénom" name="Prénom" placeholder="Prénom" required>
-            <label for="Adresse" &nbsp;</label>
-            <input type="text" id="Adresse" name="Adresse" placeholder="Adresse" required>
+
             <label for="Numéro téléphone"> &nbsp;</label>
-            <input type="text" id="Numéro téléphone" name="Numéro téléphone" placeholder="Numéro téléphone" required>
+            <input type="text" id="Numéro téléphone" name="Numéro téléphone" placeholder="Numéro téléphone"
+                   required>
             <br><br>
             Résultat recherche
             <div>
-                <textarea name="content" rows="8" cols="80" placeholder="Résultat recherches" required></textarea>
-                <div id="toolbar-editor">
-                    <div id=boutton_publier>
-                        <button class="boutton_publier" style="vertical-align:middle"><span> Publier </span></button>
-                    </div>
-                    <div id=boutton_option>
-                        <button class="boutton_option" style="vertical-align:middle">...</button>
-                    </div>
-                </div>
-            </div>
-        </form>
-        <div class="col-gauche">
-            <form id="" class="" action="index.html" method="post" accept-charset="UTF-8"
-                  enctype="multipart/form-data">
-                <h1 class="centre">Chercher un client</h1>
-                <hr>
-                <label for="Nom"> &nbsp;</label>
-                <input type="text" id="Nom" name="Nom" placeholder="Nom" required>
-                <label for="Prénom">&nbsp;</label>
-                <input type="text" id="Prénom" name="Prénom" placeholder="Prénom" required>
-
-                <label for="Numéro téléphone"> &nbsp;</label>
-                <input type="text" id="Numéro téléphone" name="Numéro téléphone" placeholder="Numéro téléphone"
-                       required>
-                <br><br>
-                Résultat recherche
-                <div>
                         <textarea name="content" rows="8" cols="80" placeholder="Résultat recherches"
                                   required></textarea>
-                    <div id="toolbar-editor">
-                        </select>
-                    </div>
+                <div id="toolbar-editor">
+                    </select>
                 </div>
-                <br><br>
-                <div class="droite">
-                    <h2>
-                        <button class="rechercher" style="vertical-align:middle"><span>Rechercher</span></button>
-                </div>
-            </form>
-        </div>
-        <div class="col-droite">
-            <div class="centre">
-
             </div>
+            <br><br>
             <div class="droite">
-                <input type="submit" name="submit" value="Exporter">
-                <input type="submit" name="submit" value="Imrpimer">
-
+                <h2>
+                    <button class="rechercher" style="vertical-align:middle"><span>Rechercher</span></button>
             </div>
+        </form>
+    </div>
+    <div class="col-droite">
+        <div class="centre">
+
+        </div>
+        <div class="droite">
+            <input type="submit" name="submit" value="Exporter">
+            <input type="submit" name="submit" value="Imrpimer">
+
         </div>
     </div>
 </div>
-</body>
