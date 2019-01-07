c31fd530eca8211a48eeea40c8d163868c5da659
bastiengrignon <basgrignon@gmail.com>
2019-01-07 10:56:44 +0100

    Add prediction when searching client on planning (ajax)

--- vue/js/main.js
+++ vue/js/main.js
@@ -1,6 +1,7 @@
 function toggleDropdown(id) {
     document.getElementById(id).classList.toggle('show');
 }
+
 closePopup(document.getElementsByClassName('close'));
 
 function closePopup(nbBtnClass) {
@@ -12,3 +13,33 @@ function closePopup(nbBtnClass) {
         });
     }
 }
+
+let search                 = document.getElementById('search-client'),
+    searchResult           = document.getElementById('result-search-client'),
+    triangle               = document.getElementsByClassName('triangle-up')[0];
+searchResult.style.display = "none";
+triangle.style.display     = "none";
+
+search.addEventListener("keyup", function () {
+    if (search.value.length !== 0) {
+        searchResult.style.display = "block";
+        triangle.style.display     = "block"
+    } else {
+        searchResult.style.display = "none";
+        triangle.style.display     = "none"
+    }
+    let xhr = new XMLHttpRequest();
+    xhr.open('GET', 'vue/db/process_planning.php?term=' + search.value, true);
+    xhr.addEventListener('readystatechange', function () {
+        if (this.readyState === 4 && this.status === 200) {
+            let result = JSON.parse(this.responseText);
+            let buffer = "";
+            for (let i = 0; i < result.length; i++) {
+                let client = result[i];
+                buffer += "<li class='dropdown-search-client'><a href='#"+client.link+"'>"+client.label+"</a></li>";
+            }
+            searchResult.innerHTML = buffer;
+        }
+    });
+    xhr.send();
+});
