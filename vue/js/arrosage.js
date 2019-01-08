6963e700dc301cbfd3bfacbe04a219646bc2084c
bastiengrignon <basgrignon@gmail.com>
2019-01-08 13:42:52 +0100

    Optimization

--- vue/js/arrosage.js
+++ vue/js/arrosage.js
@@ -2,24 +2,16 @@ let PopUpMaison   = document.getElementById('modal-add-maison');
 let PopUpZone     = document.getElementById('modal-add-zone');
 let PopUpArroseur = document.getElementById('modal-add-arroseur');
 
-let btnAddHouse    = document.getElementById('add-house');
-let btnAddZone     = document.getElementById('add-zone');
+let btnAddHouse = document.getElementById('add-house');
+let btnAddZone  = document.getElementById('add-zone');
+
+function onSelectPlanteChangeArroseurInfo() {
+    document.getElementById('form-plante-select-arr').submit();
+}
 
 showPopup(btnAddHouse, PopUpMaison);
 showPopup(btnAddZone, PopUpZone);
-/*
-closePopup(document.getElementsByClassName('close'));
-
-function closePopup(nbBtnClass) {
-    for (let i = 0; i < nbBtnClass.length; i++) {
-        let btnClose = nbBtnClass.item(i);
-        btnClose.addEventListener("click", function () {
-            // Close button need to be 4 child from modalId
-            document.getElementById(btnClose.parentNode.parentNode.parentNode.parentNode.id).style.display = "none";
-        });
-    }
-}
-*/
+modalEscape();
 
 function showPopup(BtnId, PopUpId) {
     BtnId.addEventListener('click', function () {
@@ -27,32 +19,34 @@ function showPopup(BtnId, PopUpId) {
     });
 }
 
-document.addEventListener("click", function (event) {
-    switch (event.target.id) {
-        case 'modal-add-maison':
-            PopUpMaison.style.display = "none";
-            break;
-        case 'modal-add-zone':
-            PopUpZone.style.display = "none";
-            break;
-        case 'modal-add-arroseur':
+function modalEscape() {
+    document.addEventListener("click", function (event) {
+        switch (event.target.id) {
+            case 'modal-add-maison':
+                PopUpMaison.style.display = "none";
+                break;
+            case 'modal-add-zone':
+                PopUpZone.style.display = "none";
+                break;
+            case 'modal-add-arroseur':
+                PopUpArroseur.style.display = "none";
+                break;
+        }
+    });
+    document.addEventListener("keydown", function (event) {
+        if (event.defaultPrevented) {
+            return;
+        }
+        if (event.keyCode === 27) {
             PopUpArroseur.style.display = "none";
-            break;
-    }
-});
+            PopUpMaison.style.display   = "none";
+            PopUpZone.style.display     = "none";
+        }
+    });
+}
 
-document.addEventListener("keydown", function (event) {
-    if (event.defaultPrevented) {
-        return;
-    }
-    if (event.keyCode === 27) {
-        PopUpArroseur.style.display = "none";
-        PopUpMaison.style.display   = "none";
-        PopUpZone.style.display     = "none";
-    }
-});
 
-function addArroseur(element){
+function addArroseur(element) {
     document.getElementById('zone-id').setAttribute('value', element.id.substring(14));
     PopUpArroseur.style.display = "block";
 }
