2d06c6e2adf98d79396ff13423f22543e9c2c63d
josephbsslit <joseph.bosselut@gmail.com>
2019-01-15 16:38:16 +0100

    dernier push

--- vue/CommercialClient.php
+++ vue/CommercialClient.php
@@ -22,11 +22,11 @@
             <input type="text" id="NumTel" name="NumTel" placeholder="Numéro téléphone" >
             <label for="Ville"> &nbsp;</label>
             <input type="text" id="Ville" name="Ville" placeholder="Ville" >
-          </div>
+          </div>                                                                                   // premiere recherche
                 <div>
                     <?php
                     if(isset($rows)) {
-                        foreach ($rows as $row){ ?>
+                    foreach ($rows as $row){ ?>
                             <br/>
                             <a href="index.php?cible=Commercial&fonction=accueil&id=<?php echo $row['id_util'];?>">
                                 <?php echo $row['nom_util'];
@@ -60,9 +60,9 @@
         </div>
         <div class="droite">
             <input type="submit" name="submitExport" value="Exporter">
-            <input type="submit" name="submitPrint" value="Imrpimer">
+            <input type="submit" name="submitPrint" value="Imrpimer" >
         </div>
-        <div id="ResDeRecherche">
+        <div id="ResDeRecherche">                                                       //result recherche
             <?php
                 if (isset($infos)) {
                     echo "<br/>
@@ -86,9 +86,40 @@
 </div>
 
 <script>
-
     $('#titreChamps').click(function(){
         $('#champs').toggle('fast')
     });
+
+    let csvContent = "data:text/csv;charset=utf-8,";
+    rows.forEach(function(rowArray){
+       let row = rowArray.join(",");
+       csvContent += row + "\r\n";
+    });
+
+
+
+    function imprimer_bloc(titre, objet) {
+    // Définition de la zone à imprimer
+    var zone = document.getElementById(ResDeRecherche).innerHTML;
+
+    // Ouverture du popup
+    var fen = window.open("", "", "height=500, width=600,toolbar=0, menubar=0, scrollbars=1, resizable=1,status=0, location=0, left=10, top=10");
+
+    // style du popup
+    fen.document.body.style.color = '#000000';
+    fen.document.body.style.backgroundColor = '#FFFFFF';
+    fen.document.body.style.padding = "20px";
+
+    // Ajout des données a imprimer
+    fen.document.title = Informations;
+    fen.document.body.innerHTML += " " + zone + " ";
+
+    // Impression du popup
+    fen.window.print();
+
+    //Fermeture du popup
+    fen.window.close();
+    return true;
+    }
 
 </script>
