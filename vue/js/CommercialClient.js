176e19ec365b343e5522673636f9ba08c1929f4b
josephbsslit <joseph.bosselut@gmail.com>
2019-01-15 11:22:04 +0100

    js test commercial client

--- vue/js/CommercialClient.js
+++ vue/js/CommercialClient.js
@@ -1,34 +1,38 @@
-  function makeRequest (url, tableau) {
+function makeRequest (url, tableau) {
 
-httpRequest = new XMLHttpRequest();
+  httpRequest = new XMLHttpRequest();
 
-
-    var nomClient =  document.getElementById('Nom').value;
-    alert(nomClient);
-    url += "&Nom=" + nomClient;       //concatener l'url
-
-
-
-    if (!httpRequest) {                                                                  // verif erreurs
-        alert('Abandon, impossible de créer une instance XMLHTP');
-        return false;
-    }
-
-    httpRequest.onreadystatechange = function()  { alerteContents(httpRequest);};       // fonction qui appel pour utiliser alerteContents cette fonction est appelée quand la requette est termiéne
-    httpRequest.open('GET', url, true);
-    httpRequest.send(null);
+  var nomClient =  document.getElementById('Nom').value;
+  // var PrenomClient =  document.getElementById('Prenom').value;
+  // var NumTelClient =  document.getElementById('NumeTel').value;
+  // var VilleClient =  document.getElementById('Ville').value;
+  alert(nomClient);
+  // url += "&Nom=" + nomClient;       //concatener l'url
+  // url += "&Prenom=" + PrenomClient;
+  // url += "&NumTel=" + NumTelClient;
+  // url += "&Ville=" + VilleClient;
 
 
+  if (!httpRequest) {                                                                  // verif erreurs
+      alert('Abandon, impossible de créer une instance XMLHTP');
+      return false;
   }
 
-  function alerteContents(httpRequest){
+  httpRequest.onreadystatechange = function()  { alerteContents(httpRequest);};       // fonction qui appel pour utiliser alerteContents cette fonction est appelée quand la requette est termiéne
+  httpRequest.open('GET', url, true);
+  httpRequest.send(null);
 
-    if (httpRequest.readyState == XMLHttpRequest.DONE){
-      if (httpRequest.status == 200) {
-        alert(httpRequest.responseText);
-    }
+
+}
+
+function alerteContents(httpRequest){
+
+  if (httpRequest.readyState == XMLHttpRequest.DONE){
+    if (httpRequest.status == 200) {
+        document.getElementById('ResDeRecherche').innerHTML = httpRequest.responseTexts;
   }
+}
 
 
 
-  }
+}
