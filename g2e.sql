015ed4d8d7f80fda8203710f404cc6921117145c
bastiengrignon <basgrignon@gmail.com>
2019-01-18 10:57:36 +0100

    Update database (remove your own database)

--- g2e.sql
+++ g2e.sql
@@ -3,11 +3,10 @@
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : 127.0.0.1:3306
--- Généré le :  lun. 14 jan. 2019 à 10:26
+-- Généré le :  ven. 18 jan. 2019 à 09:56
 -- Version du serveur :  5.7.19
 -- Version de PHP :  7.1.9
 
-SET FOREIGN_KEY_CHECKS = 0;
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
 START TRANSACTION;
@@ -44,9 +43,11 @@ CREATE TABLE IF NOT EXISTS `arroseur`
   `id_plante`               int(11)      NOT NULL,
   `id_type_arroseur`        int(11)      NOT NULL,
   PRIMARY KEY (`id_arr`),
-  KEY `id_zone` (`id_zone`)
+  KEY `id_zone` (`id_zone`),
+  KEY `arroseur_plante_id_plante_fk` (`id_plante`),
+  KEY `arroseur_type_arroseur_id_type_arroseur_fk` (`id_type_arroseur`)
 ) ENGINE = InnoDB
-  AUTO_INCREMENT = 13
+  AUTO_INCREMENT = 32
   DEFAULT CHARSET = utf8 COMMENT ='Arroseurs';
 
 --
@@ -55,17 +56,14 @@ CREATE TABLE IF NOT EXISTS `arroseur`
 
 INSERT INTO `arroseur` (`id_arr`, `nom_arr`, `numero_serie_arr`, `etat_arr`, `etat_fonctionnement_arr`,
                         `date_ajout_arr`, `id_zone`, `id_plante`, `id_type_arroseur`)
-VALUES (1, 'Poirier', 'DOM14250', 1, 0, '2018-11-25 17:05:00', 1, 1, 4),
-       (3, 'Noyer', 'DOM15240', 1, 0, '2018-11-21 08:44:00', 0, 1, 1),
-       (4, 'Cerisier', 'DOM15245', 1, 1, '2018-11-15 13:17:10', 1, 1, 1),
-       (5, 'Acacia', 'DOM19630', 0, 0, '2018-11-27 15:13:00', 3, 1, 2),
-       (6, 'Cocotier', 'DOM14258', 0, 2, '2018-11-27 11:10:00', 2, 1, 1),
-       (7, 'Serre', 'DOM15295', 1, 2, '2018-09-25 08:35:08', 0, 2, 2),
-       (8, 'Tomates', 'DOM15236', 1, 0, '2018-12-25 22:45:49', 1, 4, 3),
-       (9, 'Aubergines', 'DOM15237', 0, 0, '2018-12-25 22:46:29', 2, 4, 3),
-       (10, 'Plante grasse', 'DOM15278', 0, 0, '2018-12-26 18:46:57', 4, 3, 5),
-       (11, 'tomates', 'DOM12555', 1, 0, '2019-01-06 13:27:24', 4, 4, 4),
-       (12, 'Potarroseur', 'DOM11111', 0, 0, '2019-01-11 11:02:13', 1, 3, 4);
+VALUES (14, 'Poirier', 'DOM14250', 1, 0, '2018-11-25 17:05:00', 1, 1, 4),
+       (16, 'Cerisier', 'DOM15245', 0, 1, '2018-11-15 13:17:10', 1, 1, 1),
+       (17, 'Acacia', 'DOM19630', 0, 0, '2018-11-27 15:13:00', 3, 1, 2),
+       (18, 'Tomates', 'DOM15236', 1, 0, '2018-12-25 22:45:49', 1, 4, 3),
+       (20, 'Plante grasse', 'DOM15278', 1, 0, '2018-12-26 18:46:57', 6, 3, 5),
+       (21, 'Tomates', 'DOM12555', 1, 0, '2019-01-06 13:27:24', 6, 4, 4),
+       (22, 'Potarosseur', 'DOM11111', 0, 0, '2019-01-11 11:02:13', 1, 3, 4),
+       (31, 'test arroseur 2595', 'DOM95738', 0, 0, '2019-01-17 19:50:57', 3, 3, 3);
 
 -- --------------------------------------------------------
 
@@ -88,17 +86,25 @@ CREATE TABLE IF NOT EXISTS `arroseur_mode` (
 --
 
 DROP TABLE IF EXISTS `capteur`;
-CREATE TABLE IF NOT EXISTS `capteur` (
+CREATE TABLE IF NOT EXISTS `capteur`(
   `id_capt` int(11) NOT NULL AUTO_INCREMENT,
-  `numero_serie_capt` varchar(255) NOT NULL,
-  `nom_capt` varchar(255) NOT NULL,
   `type_capt` int(11) NOT NULL,
-  `valeur_capt` int(5) NOT NULL,
   `id_arr` int(11) NOT NULL,
   PRIMARY KEY (`id_capt`),
   KEY `id_arr` (`id_arr`),
   KEY `type_capt` (`type_capt`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE = InnoDB
+  AUTO_INCREMENT = 18
+  DEFAULT CHARSET = utf8;
+
+--
+-- Déchargement des données de la table `capteur`
+--
+
+INSERT INTO `capteur` (`id_capt`, `type_capt`, `id_arr`)
+VALUES (3, 3, 14),
+       (5, 7, 14),
+       (17, 3, 18);
 
 -- --------------------------------------------------------
 
@@ -264,15 +270,24 @@ CREATE TABLE IF NOT EXISTS `programme` (
 
 DROP TABLE IF EXISTS `publication`;
 CREATE TABLE IF NOT EXISTS `publication` (
-  `id_pub` int(11) NOT NULL AUTO_INCREMENT,
-  `titre_pub` varchar(255) NOT NULL,
-  `contenu_pub` text NOT NULL,
-  `fichier_pub` varchar(255) NOT NULL,
-  `date_envoi_pub` datetime NOT NULL,
-  `id_util` int(11) NOT NULL,
-  PRIMARY KEY (`id_pub`),
-  KEY `id_util` (`id_util`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+                                           `id_pub`         int(11) NOT NULL AUTO_INCREMENT,
+                                           `titre_pub`      varchar(255) NOT NULL,
+                                           `contenu_pub`    text NOT NULL,
+                                           `fichier_pub`    varchar(255) DEFAULT NULL,
+                                           `date_envoi_pub` datetime NOT NULL,
+                                           `id_util`        int(11) NOT NULL,
+                                           PRIMARY KEY (`id_pub`),
+                                           KEY `publication_utilisateur_id_util_fk` (`id_util`)
+) ENGINE = InnoDB
+  AUTO_INCREMENT = 2
+  DEFAULT CHARSET = utf8;
+
+--
+-- Déchargement des données de la table `publication`
+--
+
+INSERT INTO `publication` (`id_pub`, `titre_pub`, `contenu_pub`, `fichier_pub`, `date_envoi_pub`, `id_util`)
+VALUES (1, 'Test', '<p>New test <strong>here</strong></p>', NULL, '2019-01-18 10:12:39', 1);
 
 -- --------------------------------------------------------
 
@@ -282,46 +297,31 @@ CREATE TABLE IF NOT EXISTS `publication` (
 
 DROP TABLE IF EXISTS `ticket`;
 CREATE TABLE IF NOT EXISTS `ticket` (
-  `id_ticket` int(11) NOT NULL AUTO_INCREMENT,
-  `numero_ticket` varchar(10) NOT NULL,
-  `titre_ticket` varchar(255) NOT NULL,
-  `contenu_ticket` text NOT NULL,
-  `fichier_ticket` varchar(255) NOT NULL,
-  `date_ticket` datetime NOT NULL,
-  `id_util` int(11) NOT NULL,
-  PRIMARY KEY (`id_ticket`),
-  KEY `id_util` (`id_util`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8 COMMENT='Gestion des tickets pour les problèmes';
+                                      `id_ticket`      int(11)      NOT NULL AUTO_INCREMENT,
+                                      `titre_ticket`   varchar(255) NOT NULL,
+                                      `status_ticket`  int(11)      NOT NULL,
+                                      `contenu_ticket` text         NOT NULL,
+                                      `fichier_ticket` varchar(255) NOT NULL,
+                                      `date_ticket`    datetime     NOT NULL,
+                                      `id_util`        int(11)      NOT NULL,
+                                      PRIMARY KEY (`id_ticket`),
+                                      KEY `id_util` (`id_util`),
+                                      KEY `status_ticket` (`status_ticket`)
+) ENGINE = InnoDB
+  AUTO_INCREMENT = 5
+  DEFAULT CHARSET = utf8 COMMENT ='Gestion des tickets pour les problèmes';
 
 --
 -- Déchargement des données de la table `ticket`
 --
 
-INSERT INTO `ticket` (`id_ticket`, `numero_ticket`, `titre_ticket`, `contenu_ticket`, `fichier_ticket`, `date_ticket`, `id_util`) VALUES
-(1, '1', 'Problème arrosage plante salon', 'Bonjour, blablabla mon arroseur marche pas c\'est pas super ... et puis voilà [...] tout vas bien', '/chemin/fichier', '2019-01-04 00:53:31', 1),
-(2, '2', 'Seere en panne 5 arroseurs', 'Tous mes arroseurs dans ma serre sont en pannes', '/chemin/fichier/joint', '2019-01-04 20:00:00', 1);
-
--- --------------------------------------------------------
-
---
--- Structure de la table `type`
---
-
-DROP TABLE IF EXISTS `type`;
-CREATE TABLE IF NOT EXISTS `type` (
-  `id_type` int(11) NOT NULL,
-  `nom_type` varchar(255) NOT NULL,
-  PRIMARY KEY (`id_type`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
---
--- Déchargement des données de la table `type`
---
-
-INSERT INTO `type` (`id_type`, `nom_type`) VALUES
-(3, 'Temperature'),
-(4, 'Humidite'),
-(7, 'Presence');
+INSERT INTO `ticket` (`id_ticket`, `titre_ticket`, `status_ticket`, `contenu_ticket`, `fichier_ticket`, `date_ticket`,
+                      `id_util`)
+VALUES (3, 'Problème arrosage plante salon', 1,
+        'Bonjour, blablabla mon arroseur marche pas c\\\'est pas super ... et puis voilà [...] tout vas bien',
+        '/chemin/fichier', '2019-01-04 00:53:31', 1),
+       (4, 'Serre en panne 5 arroseurs', 2, 'Tous mes arroseurs dans ma serre sont en pannes', '/chemin/fichier/joint',
+        '2019-01-04 20:00:00', 1);
 
 -- --------------------------------------------------------
 
@@ -349,6 +349,56 @@ VALUES (1, 'Arroseur multi-surface'),
        (3, 'Aspregeur classique'),
        (4, 'Arroseur compte goutte d\'extérieur'),
        (5, 'Arroseur compte goutte d\'intérieur');
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `type_capteur`
+--
+
+DROP TABLE IF EXISTS `type_capteur`;
+CREATE TABLE IF NOT EXISTS `type_capteur`
+(
+  `id_type_capteur`  int(11)      NOT NULL,
+  `nom_type_capteur` varchar(255) NOT NULL,
+  PRIMARY KEY (`id_type_capteur`)
+) ENGINE = InnoDB
+  DEFAULT CHARSET = utf8;
+
+--
+-- Déchargement des données de la table `type_capteur`
+--
+
+INSERT INTO `type_capteur` (`id_type_capteur`, `nom_type_capteur`)
+VALUES (3, 'température'),
+       (4, 'humidité'),
+       (7, 'présence');
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `type_ticket`
+--
+
+DROP TABLE IF EXISTS `type_ticket`;
+CREATE TABLE IF NOT EXISTS `type_ticket`
+(
+  `id_type_ticket`  int(11)     NOT NULL AUTO_INCREMENT,
+  `nom_type_ticket` varchar(50) NOT NULL,
+  PRIMARY KEY (`id_type_ticket`)
+) ENGINE = InnoDB
+  AUTO_INCREMENT = 5
+  DEFAULT CHARSET = utf8 COMMENT ='Les types de tickets clients';
+
+--
+-- Déchargement des données de la table `type_ticket`
+--
+
+INSERT INTO `type_ticket` (`id_type_ticket`, `nom_type_ticket`)
+VALUES (1, 'Initialisé'),
+       (2, 'Validé'),
+       (3, 'En cours'),
+       (4, 'Finalisé');
 
 -- --------------------------------------------------------
 
@@ -417,28 +467,36 @@ INSERT INTO `utilisateur` (`id_util`, `nom_util`, `prenom_util`, `email_util`, `
 --
 
 DROP TABLE IF EXISTS `zone`;
-CREATE TABLE IF NOT EXISTS `zone` (
+CREATE TABLE IF NOT EXISTS `zone`(
   `id_zone` int(11) NOT NULL AUTO_INCREMENT,
   `nom_zone` varchar(50) NOT NULL,
   `id_habit` int(11) NOT NULL,
   PRIMARY KEY (`id_zone`),
   KEY `id_habit` (`id_habit`)
-) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8 COMMENT='Zone à pour arroseur';
+) ENGINE = InnoDB
+  AUTO_INCREMENT = 7
+  DEFAULT CHARSET = utf8 COMMENT ='Zone à pour arroseur';
 
 --
 -- Déchargement des données de la table `zone`
 --
 
 INSERT INTO `zone` (`id_zone`, `nom_zone`, `id_habit`) VALUES
-(1, 'Potager', 1),
-(2, 'Serre', 1),
-(3, 'Salon', 5),
-(4, 'Salon', 1),
-(5, 'Jardin', 5);
+                                                         (1, 'Potager', 1),
+                                                         (3, 'Salon', 5),
+                                                         (6, 'test', 1);
 
 --
 -- Contraintes pour les tables déchargées
 --
+
+--
+-- Contraintes pour la table `arroseur`
+--
+ALTER TABLE `arroseur`
+  ADD CONSTRAINT `arroseur_plante_id_plante_fk` FOREIGN KEY (`id_plante`) REFERENCES `plante` (`id_plante`),
+  ADD CONSTRAINT `arroseur_type_arroseur_id_type_arroseur_fk` FOREIGN KEY (`id_type_arroseur`) REFERENCES `type_arroseur` (`id_type_arroseur`),
+  ADD CONSTRAINT `arroseur_zone_id_zone_fk` FOREIGN KEY (`id_zone`) REFERENCES `zone` (`id_zone`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
 -- Contraintes pour la table `arroseur_mode`
@@ -452,13 +510,20 @@ ALTER TABLE `arroseur_mode`
 --
 ALTER TABLE `capteur`
   ADD CONSTRAINT `id_arr` FOREIGN KEY (`id_arr`) REFERENCES `arroseur` (`id_arr`),
-  ADD CONSTRAINT `type_capt` FOREIGN KEY (`type_capt`) REFERENCES `type` (`id_type`);
+  ADD CONSTRAINT `type_capt` FOREIGN KEY (`type_capt`) REFERENCES `type_capteur` (`id_type_capteur`);
 
 --
 -- Contraintes pour la table `donnee`
 --
 ALTER TABLE `donnee`
   ADD CONSTRAINT `id_capt` FOREIGN KEY (`id_capt`) REFERENCES `capteur` (`id_capt`);
+
+--
+-- Contraintes pour la table `habitation_utilisateur`
+--
+ALTER TABLE `habitation_utilisateur`
+  ADD CONSTRAINT `habitation_utilisateur_habitation_id_habit_fk` FOREIGN KEY (`id_habit`) REFERENCES `habitation` (`id_habit`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `habitation_utilisateur_utilisateur_id_util_fk` FOREIGN KEY (`id_util`) REFERENCES `utilisateur` (`id_util`);
 
 --
 -- Contraintes pour la table `mode`
@@ -476,26 +541,26 @@ ALTER TABLE `mode_programme`
 -- Contraintes pour la table `publication`
 --
 ALTER TABLE `publication`
-  ADD CONSTRAINT `id_util` FOREIGN KEY (`id_util`) REFERENCES `utilisateur` (`id_util`);
+  ADD CONSTRAINT `publication_utilisateur_id_util_fk` FOREIGN KEY (`id_util`) REFERENCES `utilisateur` (`id_util`);
 
 --
 -- Contraintes pour la table `ticket`
 --
 ALTER TABLE `ticket`
-  ADD CONSTRAINT `ticket_ibfk_1` FOREIGN KEY (`id_util`) REFERENCES `utilisateur` (`id_util`) ON DELETE CASCADE ON UPDATE CASCADE;
+  ADD CONSTRAINT `ticket_ibfk_1` FOREIGN KEY (`id_util`) REFERENCES `utilisateur` (`id_util`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `ticket_ibfk_2` FOREIGN KEY (`status_ticket`) REFERENCES `type_ticket` (`id_type_ticket`);
 
 --
 -- Contraintes pour la table `utilisateur`
 --
 ALTER TABLE `utilisateur`
-  ADD CONSTRAINT `utilisateur_ibfk_1` FOREIGN KEY (`type_util`) REFERENCES `type_utilisateur` (`id_type_util`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+  ADD CONSTRAINT `utilisateur_type_utilisateur_id_type_util_fk` FOREIGN KEY (`type_util`) REFERENCES `type_utilisateur` (`id_type_util`);
 
 --
 -- Contraintes pour la table `zone`
 --
 ALTER TABLE `zone`
-  ADD CONSTRAINT `zone_ibfk_1` FOREIGN KEY (`id_habit`) REFERENCES `habitation` (`id_habit`) ON DELETE CASCADE ON UPDATE CASCADE;
-SET FOREIGN_KEY_CHECKS = 1;
+  ADD CONSTRAINT `zone_habitation_id_habit_fk` FOREIGN KEY (`id_habit`) REFERENCES `habitation` (`id_habit`) ON DELETE CASCADE ON UPDATE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
