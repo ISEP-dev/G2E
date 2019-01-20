de3aba1c0c13f9f51df4551e2fe9d3dfa46db2dc
bastiengrignon <basgrignon@gmail.com>
2019-01-20 01:06:38 +0100

    Change Date format

--- modele/getusertickets.php
+++ modele/getusertickets.php
@@ -7,7 +7,7 @@ $resultQuery  = $bdd->query("SELECT * FROM ticket WHERE id_ticket='".$q."'");
 foreach ($resultQuery as $ticket) {
   echo "<div class='v-haut header-user'>ticket n°000" . $ticket['id_ticket'] . "</div>";
   echo "<br/><br/>";
-  echo "<strong> date de création de l'incident :</strong> " . $ticket['date_ticket']. "";
+    echo "<strong> Date de création de l'incident :</strong>" . date(' \L\e d M Y à H:m', strtotime($ticket['date_ticket'])) . "";
   echo "<br/><br/>";
   echo "<h3> Intitulé du ticket: </h3>".$ticket['titre_ticket'];
   echo "<br/><br/>";
@@ -15,12 +15,4 @@ foreach ($resultQuery as $ticket) {
   echo "<br/><br/>";
   echo  $ticket['fichier_ticket'];
   echo "<br/><br/>";
-
 }
-
-
-// foreach ($resultQuery as $ticket) {
-//   $jsonArray[] = array();
-// }
-// echo $resultQuery->fetch(PDO::FETCH_ASSOC);
-?>
