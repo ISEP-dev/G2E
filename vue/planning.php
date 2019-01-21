373c0fbf89c18491e1c9ba9ef95bbd4ae0cd0f8d
bastiengrignon <basgrignon@gmail.com>
2019-01-21 21:54:29 +0100

    Adding ticket feature

--- vue/planning.php
+++ vue/planning.php
@@ -9,47 +9,16 @@
     <div class="col-gauche">
         <div class="rdv">
             <div class="sticky-header-rendezvous">
-                <label>
-                    Selectionner une date :
+                <label for="calendrier">Selectionner une date :</label>
                     <input id="calendrier" type="date" name="date">
-                </label>
                 <hr>
             </div>
-            <div class="result"></div>
-            <?php
-            /*foreach($tickets as $ticket){
-                */ ?><!--
-                <div class="rendezvous space-between">
-                    <div class="nom-rendezvous">
-                        <? /*= $ticket['titre_ticket']; */ ?>
-                    </div>
-                    --><?php
-            /*            }*/
-            ?>
-            <br>
-
+            <div id="today-ticket"></div>
         </div>
     </div>
 
     <div class="col-droite">
         <h1 class="centre">Gestion des tickets</h1>
-        <?php
-        /*
-                $mysqli = new mysqli("localhost", "root", "", "g2e") or die("Erreur : " . $mysqli->error);
-                $ticketQuery = $mysqli->query("SELECT * FROM ticket WHERE id_util = " . $_SESSION['user_id']);
-                while ($tickets = $ticketQuery->fetch_assoc()) {
-                    */ ?><!--
-            <div class="rendezvous space-between">
-                <div class="nom-rendezvous">
-                    <? /*= $tickets['titre_ticket']; */ ?>
-                </div>
-                <a href="" title="Supprimer">
-                    <img class="poubelle" src="vue/images/poubelle.png" alt="poubelle.png"
-                         style="width:25px;height:25px;"/>
-                </a>
-            </div>
-            --><?php
-        /*        } # End of while*/
-        ?>
+        <div id="info-ticket"></div>
     </div>
 </div>
