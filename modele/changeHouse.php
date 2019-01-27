d5f7047e4c5f61cce42be924b8ebb35777058d24
bastiengrignon <basgrignon@gmail.com>
2019-01-27 21:26:27 +0100

    Clean-up code

--- modele/changeHouse.php
+++
@@ -1,8 +0,0 @@
-<?php
-// $base = "";
-// $data = array(
-//     'house-select' => $_POST['house-select']
-// );
-//
-// $url = $base . http_build_query($data);
-header("index.php?cible=habitation&fonction=accueil&house-select=" . $_POST['house-select']);
