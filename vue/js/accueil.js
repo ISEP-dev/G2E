080ab024c74506c934dd8ca25bb0198af3f2dc8f
bastiengrignon <basgrignon@gmail.com>
2019-02-01 23:54:42 +0100

    Verification mot de passe inscription

--- vue/js/accueil.js
+++ vue/js/accueil.js
@@ -35,3 +35,28 @@ function modalEscape() {
         }
     });
 }
+
+let psswd       = document.getElementById('password-utilisateur');
+let psswdCheck  = document.getElementById('password-utilisateur2');
+// At least 1 lowercase, 1 uppercase, 1 numeric, and 8 character
+var strongRegex = new RegExp("^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.{8,})");
+
+psswd.addEventListener("keyup", function () {
+    let res = strongRegex.test(this.value);
+    if (res) {
+        this.style.borderColor = "#72F695";
+        this.style.borderWidth = "2px";
+    } else {
+        this.style.borderColor = "#FA6A6A";
+        this.style.borderWidth = "2px";
+    }
+});
+psswdCheck.addEventListener("keyup", function () {
+    if (this.value === psswd.value || this.value === null) {
+        this.style.borderColor = "#72F695";
+        this.style.borderWidth = "2px";
+    } else {
+        this.style.borderColor = "#FA6A6A";
+        this.style.borderWidth = "2px";
+    }
+});
